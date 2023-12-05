6aa5e2c2759e554c0693b74018cc4309a34ecf99
Matthias Hamacher <m.hamacher@basecom.de>
2023-12-05 19:02:15 +0100

    Solve Day 5, Create file for Day 6

--- day-6.py
+++ day-6.py
@@ -5,94 +5,20 @@ from aocd.models import Puzzle
 from aocd.examples import Example
 
 ## Packages to solve the puzzle
-import re
 
 
 def parse(puzzle_input):
-    input_array = puzzle_input.split('\n\n')
+    input_array = puzzle_input.split('\n')
 
-    result = {}
-
-    for input_line in input_array:
-        if re.match(r'seeds: ([\d+\s]+)', input_line):
-            seeds = re.match(r'seeds: ([\d+\s]+)', input_line).group(1).split()
-            result['seeds'] = seeds
-        elif re.match(r'[a-z]+-[a-z]+-[a-z]+ map:.*', input_line):
-            line_parts = input_line.split('\n')
-            map_name = re.match(r'([a-z]+-[a-z]+-[a-z]+) map:.*', line_parts[0]).group(1)
-            map_array = []
-
-            for line in line_parts[1:]:
-                [destination, source, range] = line.split()
-                map_array.append([int(source), int(destination), int(range)])
-
-            result[map_name] = map_array
-
-    return result
+    return input_array
 
 
 def part_a(data):
-    lowest_location = 0
-    seeds = data['seeds']
-    maps = list(data.items())[1:]
-
-    for seed in seeds:
-        seed = int(seed)
-
-        for map in maps:
-            for [source, destination, range] in map[1]:
-                if seed < source or seed > (source + range):
-                    continue
-                elif seed <= (source + range):
-                    seed = destination + (seed - source)
-                    break
-
-        if seed < lowest_location or lowest_location == 0:
-            lowest_location = seed
-
-    return lowest_location
+    """Part A"""
 
 
 def part_b(data):
-    all_seeds = [int(seed) for seed in data['seeds']]
-    maps = list(data.items())[1:]
-
-    seeds = list(map(lambda i: range(all_seeds[i], all_seeds[i] + all_seeds[i + 1]), range(0, len(all_seeds), 2)))
-
-    current_gen = seeds
-
-    for map_definition in maps:
-        found = []
-        pre_search = current_gen
-
-        for [source, destination, map_range_value] in map_definition[1]:
-            map_range = range(source, source + map_range_value)
-            offset = destination - source
-
-            new_unconverted = []
-
-            for r in pre_search:
-                overlap = range(r.start, r.start)
-
-                if r.start < map_range.stop and r.stop >= map_range.start:
-                    overlap = range(max(r.start, map_range.start), min(r.stop, map_range.stop))
-
-                left = range(r.start, overlap.start)
-                if left.stop > left.start:
-                    new_unconverted.append(left)
-
-                if overlap.stop > overlap.start:
-                    found.append(range(overlap.start + offset, overlap.stop + offset))
-
-                right = range(overlap.stop, r.stop)
-                if right.stop > right.start:
-                    new_unconverted.append(right)
-
-            pre_search = new_unconverted
-
-        current_gen = found + pre_search
-
-    return min(map(lambda r: r.start, current_gen))
+    """Part B"""
 
 
 def test(examples: List[Example], solve_part_a=True, solve_part_b=True):
@@ -153,7 +79,7 @@ def solve(puzzle: Puzzle, solve_part_a=True, solve_part_b=True, submit_solution=
 
 
 if __name__ == "__main__":
-    puzzle = Puzzle(year=2023, day=5)
+    puzzle = Puzzle(year=2023, day=6)
 
     localzone = datetime.now().astimezone().tzinfo
     now = datetime.now().astimezone(tz=localzone)
