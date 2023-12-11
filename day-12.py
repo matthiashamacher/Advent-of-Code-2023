271e614259cd699dfb12381b1369ccd70a8a12f1
Matthias Hamacher <m.hamacher@basecom.de>
2023-12-11 12:59:43 +0100

    Solve Day 11, Create file for Day 12

--- day-12.py
+++ day-12.py
@@ -5,62 +5,20 @@ from aocd.models import Puzzle
 from aocd.examples import Example
 
 ## Packages to solve the puzzle
-import numpy as np
+
 
 def parse(puzzle_input):
     input_array = puzzle_input.split('\n')
-    result = []
 
-    for line in input_array:
-        result.append([*line.strip()])
-
-    return result
+    return input_array
 
 
 def part_a(data):
-    values = np.array(data)
-    val = np.zeros(values.shape, dtype=int)
-    val[np.where(values == '#')] = 1
-    val_new = []
-
-    for row in val:
-        val_new.append(row)
-        if np.sum(row) == 0:
-            val_new.append(row)
-
-    val = np.array(val_new).transpose((1, 0)).copy()
-    val_new = []
-
-    for row in val:
-        val_new.append(row)
-        if np.sum(row) == 0:
-            val_new.append(row)
-
-    val = np.array(val_new).transpose((1, 0)).copy()
-    hits = np.where(val != 0)
-    hits = np.array(hits)
-    sol = 0
-
-    for i in range(np.array(hits).shape[1]):
-        for j in range(i+1, np.array(hits).shape[1]):
-            sol += np.sum(np.abs(hits[:, i] - hits[:, j]))
-
-    return sol
+    """Part A"""
 
 
 def part_b(data):
-    values = np.array(data)
-    val = np.zeros(values.shape, dtype=int)
-    val[np.where(values == '#')] = 1
-    hits = np.where(val != 0)
-    hits = np.array(hits)
-    sol2 = 0
-
-    for i in range(np.array(hits).shape[1]):
-        for j in range(i+1, np.array(hits).shape[1]):
-            sol2 += np.sum(np.abs(hits[:, i] - hits[:, j]))
-
-    return (9599070 - sol2) * 999999 + sol2
+    """Part B"""
 
 
 def test(examples: List[Example], solve_part_a=True, solve_part_b=True):
@@ -84,19 +42,16 @@ def test(examples: List[Example], solve_part_a=True, solve_part_b=True):
                     print(f"Part A: ERROR, expected {example.answer_a}, got {result}")
 
         if solve_part_b:
-            # Part B example does not work as we have no final solution in the html
-            # therefore we cannot automatically test it
-            part_b_success = True
-            # if example.answer_b is None:
-            #     print(f"Part B: No example solution provided.")
-            # else:
-            #     result = part_b(data)
-            #
-            #     if result == int(example.answer_b):
-            #         print("Part B: OK")
-            #         part_b_success = True
-            #     else:
-            #         print(f"Part B: ERROR, expected {example.answer_b}, got {result}")
+            if example.answer_b is None:
+                print(f"Part B: No example solution provided.")
+            else:
+                result = part_b(data)
+
+                if result == int(example.answer_b):
+                    print("Part B: OK")
+                    part_b_success = True
+                else:
+                    print(f"Part B: ERROR, expected {example.answer_b}, got {result}")
 
     return part_a_success, part_b_success
 
@@ -124,7 +79,7 @@ def solve(puzzle: Puzzle, solve_part_a=True, solve_part_b=True, submit_solution=
 
 
 if __name__ == "__main__":
-    puzzle = Puzzle(year=2023, day=11)
+    puzzle = Puzzle(year=2023, day=12)
 
     localzone = datetime.now().astimezone().tzinfo
     now = datetime.now().astimezone(tz=localzone)
